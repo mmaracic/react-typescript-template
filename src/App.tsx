5fa96b39f6dd97450479548420a95434ed5f2855
mmaracic <marijo.maracic@gmail.com>
2025-03-20 01:38:47 +0100

    Replaced styled-components with pandacss, rebuild and move header to pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import { Theme } from '@radix-ui/themes'
+import "@radix-ui/themes/styles.css";
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import Header from './components/Header'
+import Header from './pages/Header'
+import { css } from '../styled-system/css';
 
 function App() {
-  const baseUrl = import.meta.env.VITE_SERVER + "/messages"
+  const appTitle = import.meta.env.VITE_APP_TITLE
 
 
 
   return (
     <Theme>
       <Header />
+      <img src={reactLogo} alt="react logo" />
+      <img src={viteLogo} alt="vite logo" />
+      <div className={css({ color: "blue", fontSize: "2xl", fontWeight: 'bold' })}>Hello 🐼 from {appTitle}!</div>
     </Theme>
   )
 }
